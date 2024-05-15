048cc98710bc90bee302e0e85298c550da528fe4
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-05-15 11:25:27 +0900

    vuetifyの設定ファイルの追加

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,23 @@
+// Vuetify
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify(
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  {
+    components,
+    directives,
+    theme: {
+      defaultTheme: 'light',
+      themes: {
+        light: {
+          colors: {
+            primary: '#009688'
+          }
+        }
+      }
+    }
+  }
+)
