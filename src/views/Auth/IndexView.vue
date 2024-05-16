74ecd81cd5cbf0c872eeed79e1c28182033bdd5c
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-05-16 18:01:30 +0900

    【認証画面】一部デザインの共通化

--- src/views/Auth/IndexView.vue
+++ src/views/Auth/IndexView.vue
@@ -1,4 +1,30 @@
 <script setup></script>
 <template>
-  <RouterView />
+  <v-container fluid fill-height>
+    <v-row class="grid-box" align="center" justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <div class="text-center">
+          <img height="70" contain src="@/assets/alt_logo.png" />
+        </div>
+        <RouterView />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+<style scoped>
+.grid-box {
+  height: 100vh;
+}
+.zoom-in-enter-active {
+  animation: zoom-in 0.5s;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0.5);
+  }
+  to {
+    transform: scale(1);
+  }
+}
+</style>
