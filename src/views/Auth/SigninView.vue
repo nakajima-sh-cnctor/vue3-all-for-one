571bf788e3a4ac432d664e8191b52792968af789
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-05-16 15:38:21 +0900

    【認証画面】細かなデザイン調整

--- src/views/Auth/SigninView.vue
+++ src/views/Auth/SigninView.vue
@@ -27,8 +27,8 @@ const v$ = reactive(
         <div class="text-center">
           <img height="70" contain src="@/assets/alt_logo.png" transition="fab-transition" />
         </div>
-        <v-card class="mx-auto my-3" style="max-width: 500px">
-          <v-card-title>サインイン</v-card-title>
+        <v-card class="mx-auto my-3 pa-5" style="max-width: 500px">
+          <v-card-title class="text-center">サインイン</v-card-title>
           <v-card-item>
             <v-text-field
               v-model="formValue.email"
@@ -41,6 +41,7 @@ const v$ = reactive(
               :error-messages="v$.email.$errors.map((e) => e.$message)"
               @input="v$.email.$touch"
               @blur="v$.email.$touch"
+              class="my-3"
             ></v-text-field>
 
             <v-text-field
@@ -55,6 +56,7 @@ const v$ = reactive(
               :error-messages="v$.password.$errors.map((e) => e.$message)"
               @input="v$.password.$touch"
               @blur="v$.password.$touch"
+              class="my-3"
             ></v-text-field>
           </v-card-item>
           <v-card-item>
