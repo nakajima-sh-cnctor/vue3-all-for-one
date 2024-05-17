7102e1c34d26e3b33f8498e4ca36261a3115fb25
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-05-18 00:19:25 +0900

    【OverrideVuetify】v-text-fieldをオーバーライドし共通部分をまとめたコンポーネントを追加

---
+++ src/components/OverrideVuetify/TextFieldComponent.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <v-text-field
+    v-bind="$attrs"
+    v-on="$attrs"
+    variant="outlined"
+    density="compact"
+    color="primary"
+    single-line
+    class="my-3"
+  ></v-text-field>
+</template>
