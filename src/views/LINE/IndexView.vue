d8dcca669eae3672636123bf45a1f124e7d8d6ed
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-05-21 16:47:39 +0900

    【LINE管理】Indexの追加

---
+++ src/views/LINE/IndexView.vue
@@ -0,0 +1,16 @@
+<script setup>
+// コンポーネント
+import CHeader from '@/components/HeaderComponent.vue'
+import CMenu from '@/components/MenuComponent.vue'
+</script>
+<template>
+  <v-app class="v-app">
+    <c-header></c-header>
+    <v-main>
+      <c-menu></c-menu>
+      <div class="pa-5">
+        <RouterView />
+      </div>
+    </v-main>
+  </v-app>
+</template>
