ac36adb609a47439b48726cfe391ab8a0c74c46d
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-05-22 16:08:17 +0900

    【コンポーネント化】入力項目のキャプションのコンポーネント化

---
+++ src/components/CaptionComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  caption: {
+    type: String,
+    required: true
+  }
+})
+</script>
+<template>
+  <div>
+    <p class="font-weight-bold text-caption text-secondary">{{ caption }}</p>
+    <!-- キャプションが必要な入力フォームをスロットに入れる -->
+    <slot></slot>
+  </div>
+</template>
