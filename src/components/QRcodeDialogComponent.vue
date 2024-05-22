73a13c8ce2ddd4afa41cb729709f23f0754210cc
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-05-22 16:11:49 +0900

    【コンポーネント化】QRコードを表示するダイアログのコンポーネント化

---
+++ src/components/QRcodeDialogComponent.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, reactive } from 'vue'
+import VueQrcode from '@chenfengyuan/vue-qrcode'
+
+const generateText = ref('https://line.me/R/ti/p/@xxxxxxx')
+const option = reactive({
+  width: 200,
+  height: 200,
+  color: {
+    dark: '#009688',
+    light: '#ffffff'
+  }
+})
+</script>
+<template>
+  <v-dialog max-width="400">
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn color="primary" class="my-3" variant="flat" v-bind="activatorProps">
+        <v-icon>mdi-qrcode</v-icon>
+        QRコードを表示
+      </v-btn>
+    </template>
+
+    <template v-slot:default="{ isActive }">
+      <v-card title="LINE公式アカウント">
+        <v-card-item class="text-center">
+          <vue-qrcode
+            v-if="generateText"
+            :value="generateText"
+            :options="option"
+            tag="img"
+          ></vue-qrcode>
+        </v-card-item>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text="閉じる" @click="isActive.value = false" color="primary"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
