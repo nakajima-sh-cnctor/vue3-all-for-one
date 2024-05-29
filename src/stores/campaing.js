abaec8abc56e1a74bcd293be507337202bdbb384
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-05-29 16:49:29 +0900

    【pinia】キャンペーンの状態管理を追加

---
+++ src/stores/campaing.js
@@ -0,0 +1,28 @@
+import { ref, reactive } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCampaing = defineStore('campaingStore', () => {
+  /**
+   * State
+   * サービス情報
+   * @type{object}
+   */
+  const headers = reactive([
+    {
+      align: 'start',
+      key: 'name',
+      sortable: false,
+      title: 'キャンペーン名'
+    },
+    { key: 'count', title: 'メッセージ数' },
+    { key: 'operation', title: '操作' }
+  ])
+  const desserts = reactive([])
+  const page = ref(1)
+
+  return {
+    headers,
+    desserts,
+    page
+  }
+})
